88c91b468eed6d61902c7d7771d7f846f9d81751
yiwahpsp <patsaporn.kaewkong@gmail.com>
2025-08-29 18:00:01 +0700

    fix: change model for be

--- dashboard-backend/models/sensorData.js
+++ dashboard-backend/models/sensorData.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 
 const sensorDataSchema = new mongoose.Schema({
-  heartRate: String,
-  // motion: String,
-  gyroX: Number,
-  gyroY: Number,
-  gyroZ: Number,
+  heartRate: Number,
+  motion: Boolean,
   humid: Number,
   temp: Number,
   sound: Number,
