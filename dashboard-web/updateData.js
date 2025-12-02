34386f9c4aed66733fe96aeb664439cc722f8a92
Piinxluna <pingpingkuss@gmail.com>
2025-12-02 16:28:17 +0700

    fix: delete unnecessary string

--- dashboard-web/updateData.js
+++ dashboard-web/updateData.js
@@ -5,28 +5,30 @@ const backendUrl = `https://iotsystems-sleepduck-backend.onrender.com`
 
 lightOnInput.addEventListener('change', () => {
   const [hour, minute] = lightOnInput.value.split(':').map(Number)
+
   fetch(`${backendUrl}/data/setting`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      lightOnHour: hour.toString(),
-      lightOnMin: minute.toString(),
+      lightOnHour: hour,
+      lightOnMin: minute,
     }),
   })
 })
 
 lightOffInput.addEventListener('change', () => {
   const [hour, minute] = lightOffInput.value.split(':').map(Number)
+
   fetch(`${backendUrl}/data/setting`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      lightOffHour: hour.toString(),
-      lightOffMin: minute.toString(),
+      lightOffHour: hour,
+      lightOffMin: minute,
     }),
   })
 })
